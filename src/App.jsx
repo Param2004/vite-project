ab0aed107bd16c49f35178f329f2de5d4d4933e4
Param sharma <kaushik.param2004@gmail.com>
2024-03-07 08:51:54 +0530

    save

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Download File</h1>
+      <div className="card">
+        <button className='btn'>
+        <img className='down' src='https://cdn-icons-png.flaticon.com/64/189/189249.png' alt='link'></img>
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
