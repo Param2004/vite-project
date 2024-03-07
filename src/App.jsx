41a3d19de501ed22b0e0521830f58e927cd90277
Param sharma <kaushik.param2004@gmail.com>
2024-03-07 09:33:11 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 
 import './App.css'
+import FileDownload from './download'
 
 function App() {
 
   return (
     <>
-      <h1>Download File</h1>
-      <div className="card">
-        <button className='btn'>
-        <img className='down' src='https://cdn-icons-png.flaticon.com/64/189/189249.png' alt='link'></img>
-        </button>
-      </div>
+      <FileDownload />
     </>
   )
 }
