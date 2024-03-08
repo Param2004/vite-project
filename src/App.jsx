83dc39e4973e3a517e771c2f31d8a2ae91b23b57
Param sharma <kaushik.param2004@gmail.com>
2024-03-09 00:51:24 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 
 import './App.css'
-import FileDownload from './component/download'
 
 function App() {
 
+  const handleDownload = () => {
+    // Replace 'example.txt' with the actual file name and extension
+    const fileName = 'translated.jpeg';
+
+    // Set the file path
+    const filePath = 'src/' + fileName;
+
+    // Create a link element
+    const downloadLink = document.createElement('a');
+
+    // Set the download link's properties
+    downloadLink.href = filePath;
+    downloadLink.download = fileName;
+
+    // Append the link to the body
+    document.body.appendChild(downloadLink);
+
+    // Trigger the click event on the link
+    downloadLink.click();
+
+    // Remove the link from the body
+    document.body.removeChild(downloadLink);
+  };
+
   return (
     <>
-      <FileDownload />
+    <div>
+      <a href='/img.pptx' target='_blank'><button onClick={handleDownload}>Download File</button></a>
+    </div>
     </>
   )
 }
