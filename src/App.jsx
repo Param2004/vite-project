18b0ea181960a9833f564a68b7dbb44f1584c37a
Param sharma <kaushik.param2004@gmail.com>
2024-03-09 00:56:31 +0530

    sav

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     const fileName = 'translated.jpeg';
 
     // Set the file path
-    const filePath = 'src/' + fileName;
+    const filePath = 'public/' + fileName;
 
     // Create a link element
     const downloadLink = document.createElement('a');
